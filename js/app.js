db0e2bae48d33f3b3eebd58ba4070835a8e0fc1d
ShihabReza <shihabreza457@gmail.com>
2022-02-28 21:53:54 +0600

    ad-fech

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
-console.log('The Website');
+const searchBtn = ()=>{
+    const inputType = document.getElementById('inputFild');
+    const inputValue = inputType.value;
+    inputType.value=""
+    const url = `https://openapi.programming-hero.com/api/phones?search=${inputValue}`
+    fetch(url)
+    .then(response => response.json())
+    .then(data=>diplayInput(data.data))
+}
+const diplayInput=(phones)=>{
+    const container = document.getElementById('main-content')
+    phones.forEach((phone)=>{
+        console.log(phone)
+       const div = document.createElement('div')
+       div.innerHTML=`
+       <div class="card" style="width: 18rem;">
+       <img src="..." class="card-img-top" alt="...">
+       <div class="card-body">
+         <h5 class="card-title">Card title</h5>
+         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+         <a href="#" class="btn btn-primary">Go somewhere</a>
+       </div>
+     </div>
+       ` 
+    })
+}
