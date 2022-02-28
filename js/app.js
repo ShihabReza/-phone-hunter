713a4c9becee25b6ee246940d68acb5f7e2d7771
ShihabReza <shihabreza457@gmail.com>
2022-02-28 22:11:59 +0600

    add-ul

--- js/app.js
+++ js/app.js
@@ -12,15 +12,18 @@ const diplayInput=(phones)=>{
     phones.forEach((phone)=>{
         console.log(phone)
        const div = document.createElement('div')
+       div.style.marginTop=('25px')
+       div.className='col-xl-4'
        div.innerHTML=`
-       <div class="card" style="width: 18rem;">
-       <img src="..." class="card-img-top" alt="...">
-       <div class="card-body">
-         <h5 class="card-title">Card title</h5>
-         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-         <a href="#" class="btn btn-primary">Go somewhere</a>
-       </div>
-     </div>
+        <div class="card" style="width: 18rem ;">
+          <img src="${phone.image}" class="card-img-top" alt="...">
+             <div class="card-body">
+             <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
        ` 
+       container.appendChild(div)
     })
 }
