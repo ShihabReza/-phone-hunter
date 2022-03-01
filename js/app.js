ac75043dbc05020becbdd85cdaa16c744caacac1
ShihabReza <shihabreza457@gmail.com>
2022-03-01 20:41:37 +0600

    add-detils-info

--- js/app.js
+++ js/app.js
@@ -4,9 +4,10 @@ const searchBtn = ()=>{
    
     
     const inputType = document.getElementById('inputFild');
+    
     const inputValue = inputType.value;
     inputType.value=""
-
+    
     const url = `https://openapi.programming-hero.com/api/phones?search=${inputValue}`
     fetch(url)
     .then(response => response.json())
@@ -16,16 +17,19 @@ const searchBtn = ()=>{
 const diplayInput=(phones)=>{
     
     const container = document.getElementById('main-content')
-    if(!phones){
-        console.log("Hello! I am an alert box!!");
-    }
-    phones?.forEach((phone)=>{
+    // if(!phones){
+    //     console.log("Hello! I am an alert box!!");
+    // }
+    phones.forEach((phone)=>{
         console.log(phone)
        const div = document.createElement('div')
        div.style.marginTop=('25px')
+       div.style.textAlign=('center')
        div.className='col-12 col-xl-4'
+       div.style.borderRadius=('25px')
+      
        div.innerHTML=`
-        <div class="card" style="width: 18rem ;">
+        <div class="" style="width: 18rem ;">
           <img src="${phone.image}" class="card-img-top" alt="...">
              <div class="card-body">
              <h5 class="card-title">${phone.phone_name}</h5>
@@ -50,13 +54,17 @@ const setInfo=(info)=>{
    console.log(info)
    const detalsInfo = document.getElementById('detls-info')
    const div = document.createElement('div')
+   
+  
    div.innerHTML=`
-     <div class="card" style="width: 18rem;">
+     <div class="" style="width: 18rem ;">
      <img src="${info.image}" class="card-img-top" alt="...">
        <div class="card-body">
-       <h1>${info.mainFeatures.memory}</h1>
-       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+       <h1>ReleaseDate: ${info.releaseDate}</h1>
+       <p>ChipSet: ${info.mainFeatures.chipSet}</p>
+       <p>DisplaySize: ${info.mainFeatures.displaySize}</p>
+       <p>Memory: ${info.mainFeatures.memory}</p>
+      
     </div>
  </div>
    `
